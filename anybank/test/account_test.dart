5a1db6bca69305bb0e44da025d94ee061f3d70b7
Thiago Fernando <thiago.fernando@msn.com>
2024-08-05 19:45:29 -0400

    modulo 2

--- anybank/test/account_test.dart
+++ anybank/test/account_test.dart
@@ -2,12 +2,33 @@ import 'package:anybank/models/account.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late Account account;
+  setUp(
+    () {
+      account = Account(id: 123, name: 'Maria', balance: 100, cpf: '123.123.123-09');
+    },
+  );
   group('Testes de Transferencias', () {
-    test('Testa uma transferencia com sucesso', () {});
-    test('Testa uma transferencia com valor negativo', () {});
+    test('Testa uma transferencia com sucesso', () {
+      account.transfer(100);
+      expect(account.balance, 0);
+    });
+    test('Testa uma transferencia com valor negativo', () {
+      account.transfer(-100);
+      expect(account.balance, 100);
+    });
     test('Transfere qualquer coisa que nao seja numeros', () {});
-    test('Testa uma transferencia com valor maior que o disponivel', () {});
-    test('Testa uma transferencia com valor zero', () {});
-    test('Testa uma transferencia com valor null', () {});
+    test('Testa uma transferencia com valor maior que o disponivel', () {
+      account.transfer(110);
+      expect(account.balance, 100);
+    });
+    test('Testa uma transferencia com valor zero', () {
+      account.transfer(0);
+      expect(account.balance, 100);
+    });
+    test('Testa uma transferencia com valor null', () {
+      account.transfer(null);
+      expect(account.balance, 100);
+    });
   });
 }
